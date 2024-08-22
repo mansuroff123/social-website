0c7920c57d2c7a18738884fc9e22a140db63786f
mansuroff123 <mansuroff145@gmail.com>
2024-08-22 18:50:17 +0500

    social website views.py writing...

---
+++ account/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from .models import Profile
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.Form):
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Repeat password',
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'firstname', 'lastname']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Password don't match!")
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'firstname', 'lastname']
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
