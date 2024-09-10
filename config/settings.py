25bbaa78f31576bc90d6d521b4f2ec942b0f4aee
mansuroff123 <mansuroff145@gmail.com>
2024-09-10 18:38:47 +0500

    google auth

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = BASE_DIR / 'assets',
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
